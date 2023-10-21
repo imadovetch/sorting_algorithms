ddf15d036282386376f5c8e5124cd18e3c9fbeb2
imadovetch <ibouderoua63@gmail.com>
2023-10-21 14:49:36 +0100

    first

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,20 +8,6 @@
  *
  * Return: Nothing
  */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j ,tmp;
@@ -42,16 +28,4 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 	}
-}
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
 }
