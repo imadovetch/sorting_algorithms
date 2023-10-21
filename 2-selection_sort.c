9091c8b2e32185fa4778a2643b0621d76789558d
imadovetch <ibouderoua63@gmail.com>
2023-10-21 15:52:37 +0100

    secod

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,6 +7,21 @@
  *
  * Return: Always 0
  */
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
 void selection_sort(int *array, size_t size) {
     size_t i, j, min_index;
     int temp;
@@ -24,18 +39,6 @@ void selection_sort(int *array, size_t size) {
             array[i] = array[min_index];
             array[min_index] = temp;
         }
+        print_array(array,size);
     }
-}
-
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    selection_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
 }
