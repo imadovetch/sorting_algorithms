c9909e22e7ce55b035123e24594b65a8e6dbfca4
imadovetch <ibouderoua63@gmail.com>
2023-10-21 15:37:21 +0100

    second

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
-
-/**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
-listint_t *create_listint(const int *array, size_t size)
-{
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
-
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
-}
-
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers
@@ -43,54 +9,33 @@ listint_t *create_listint(const int *array, size_t size)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *nxt, *curr;
+	listint_t *after, *current;
 
 	if (!list || !*list || !(*list)->next)
 		return;
 
-	curr = *list;
-	while (curr)
+	current = *list;
+	while (current)
 	{
-		nxt = curr->next;
-		while (curr->prev && curr->n < curr->prev->n) /* N<-5<->4<->3<->2<->1<->N */
+		after = current->next;
+		while (current->prev && current->n < current->prev->n)
 		{
-			curr->prev->next = curr->next;
-			if (curr->next)
-				curr->next->prev = curr->prev;
+			current->prev->next = current->next;
+			if (current->next)
+				current->next->prev = current->prev;
 
-			curr->next = curr->prev;
-			curr->prev = curr->prev->prev;
-			curr->next->prev = curr;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
 
-			if (curr->prev)
-				curr->prev->next = curr;
+			if (current->prev)
+				current->prev->next = current;
 			else
-				*list = curr;
+				*list = current;
 
 			print_list(*list);
 
 		}
-		curr = nxt;
+		current = after;
 	}
-}
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    listint_t *list;
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    list = create_listint(array, n);
-    if (!list)
-        return (1);
-    print_list(list);
-    printf("\n");
-    insertion_sort_list(&list);
-    printf("\n");
-    print_list(list);
-    return (0);
 }
